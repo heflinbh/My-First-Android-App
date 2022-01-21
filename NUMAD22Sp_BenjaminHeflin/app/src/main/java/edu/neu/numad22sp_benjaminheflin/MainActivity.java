f299ad136b09c58aab56da56b8471c25942d19cf
heflinbh <76850289+heflinbh@users.noreply.github.com>
2022-01-21 15:34:41 -0500

    Introduction Succeeded
    Can take input name an personally introduce myself

--- NUMAD22Sp_BenjaminHeflin/app/src/main/java/edu/neu/numad22sp_benjaminheflin/MainActivity.java
+++ NUMAD22Sp_BenjaminHeflin/app/src/main/java/edu/neu/numad22sp_benjaminheflin/MainActivity.java
@@ -3,6 +3,9 @@ package edu.neu.numad22sp_benjaminheflin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void introduce (View view) {
+
+        EditText personNameText = findViewById(R.id.personName);
+
+        TextView textView = findViewById(R.id.textView);
+
+        if (!personNameText.equals("")) {
+            textView.setText("Hello " + personNameText.getText().toString() + "!" +
+                             "\nMy name is Benjamin Heflin. Nice to meet you!" +
+                             "\nFeel free to reach me at heflin.be@northeastern.edu" +
+                             "\nSee you around!"
+
+
+            /*textView.setText("Hello" + personNameText.getText().toString()*/);;
+        }
+
+        else {
+            textView.setText("Benjamin Heflin's App");
+        }
     }
 }
