55fe596ffcbf8eaa60534d3c6721c8e71b298837
heflinbh <76850289+heflinbh@users.noreply.github.com>
2022-02-04 12:12:56 -0500

    New activity, cleaned onClick
    On Click now uses a switch case structure. New button "ClickyClicky" added, which eventually will activate a new activity.

---
+++ NUMAD22Sp_BenjaminHeflin/app/src/main/java/edu/neu/numad22sp_benjaminheflin/ClickyActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.numad22sp_benjaminheflin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky);
+    }
+}
--- NUMAD22Sp_BenjaminHeflin/app/src/main/java/edu/neu/numad22sp_benjaminheflin/MainActivity.java
+++ NUMAD22Sp_BenjaminHeflin/app/src/main/java/edu/neu/numad22sp_benjaminheflin/MainActivity.java
@@ -6,30 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void introduce (View view) {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.AboutMeButton:
+                EditText personNameText = findViewById(R.id.personName);
+                TextView textView = findViewById(R.id.textView);
+                if (!personNameText.getText().toString().equals("")) {
+                    textView.setText("Howdy " + personNameText.getText().toString() + "!" +
+                            "\nMy name is Benjamin Heflin. Nice to meet you!" +
+                            "\nFeel free to reach me at heflin.be@northeastern.edu" +
+                            "\nSee you around!");
+                } else {
+                    textView.setText("Hello World!");
+                }
+                break;
 
-        EditText personNameText = findViewById(R.id.personName);
-
-        TextView textView = findViewById(R.id.textView);
-
-        if (!personNameText.getText().toString().equals("")) {
-            textView.setText("Howdy " + personNameText.getText().toString() + "!" +
-                             "\nMy name is Benjamin Heflin. Nice to meet you!" +
-                             "\nFeel free to reach me at heflin.be@northeastern.edu" +
-                             "\nSee you around!");
+            case R.id.ClickyClicky:
+                Toast.makeText(getApplicationContext(), "Success!!!", Toast.LENGTH_SHORT).show();
+                break;
         }
 
-        else {
-            textView.setText("Hello World!");
-        }
+
     }
+
 }
